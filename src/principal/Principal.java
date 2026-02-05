fa8951bef64a8894f18596a47b7812954c945f80
karime.a.p <karime.a.p@localhost>
2026-02-05 16:38:54 -0600

    Cambio1

---
+++ src/principal/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package principal;
+
+/**
+ *
+ * @author karime.a.p
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Integrante:");
+        System.out.println("1. Karim Eduardo Aguilera Portillo - 22550324");
+        System.out.println("2. Aaron Manuel Diaz Ornelas");
+        System.out.println("3. Sinue Abelardo Mendoza Portillo");
+        System.out.println("3. Angel");
+    }
+}
