2de809f4a59aebeb626a688e3bd8c94535eb95ae
KarimAP08 <124391455+KarimAP08@users.noreply.github.com>
2026-02-05 14:16:03 -0600

    Add files via upload

---
+++ Principal/src/principal/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package principal;
+
+/**
+ *
+ * @author karime.a.p
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Integrante:");
+        System.out.println("1. Karim Eduardo Aguilera Portillo - 22550324");
+    }
+}
