f1795b099cb885d219b57407a4a09fd3344fbd25
KarimAP08 <124391455+KarimAP08@users.noreply.github.com>
2026-02-05 14:31:03 -0600

    Add files via upload

--- Principal/src/principal/Principal.java
+++ Principal/src/principal/Principal.java
@@ -12,5 +12,7 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Integrante:");
         System.out.println("1. Karim Eduardo Aguilera Portillo - 22550324");
+        System.out.println("2. Aaron Manuel Diaz Ornelas");
+        System.out.println("3. Sinue Abelardo Mendoza Portillo");
     }
 }
